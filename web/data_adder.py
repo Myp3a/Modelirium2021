602033f44bce2931d38bcc6fa0001fd2b001847e
Myp3a <murza8965@gmail.com>
2021-06-20 04:08:52 +0500

    Improve data generation

--- web/data_adder.py
+++ web/data_adder.py
@@ -4,14 +4,8 @@ import random
 import time
 
 uid = sys.argv[1]
-count = int(sys.argv[2])
-try:
-    state = sys.argv[3]
-except:
-    state = None
 
-#for i in range(count):
 while True:
     with requests.Session() as sess:
-        sess.post(f'http://127.0.0.1:8080/{uid}/data',data={'upper':random.randint(100,160),'lower':random.randint(70,110),'pulse':random.randint(50,85),'state':state})
+        sess.post(f'http://127.0.0.1:8080/{uid}/data',data={'upper':random.randint(100,160),'lower':random.randint(70,110),'pulse':random.randint(50,85))
         time.sleep(1)
